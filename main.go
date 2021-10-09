e5b00e933ea5d81687ae285ef3917c2a84c36771
ekokurniadi <ekokurniadi.02@gmail.com>
2021-10-09 14:41:29 +0700

    mempersiapkan folder,file dan struct untuk endpoint transaction

--- main.go
+++ main.go
@@ -45,6 +45,7 @@ func main() {
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
 	api.POST("/avatars", authMiddleware(authService, userService), userHandler.UploadAvatar)
+
 	api.GET("/campaigns", campaignHandler.GetCampaigns)
 	api.GET("/campaigns/:id", campaignHandler.GetCampaign)
 	api.POST("/campaigns", authMiddleware(authService, userService), campaignHandler.CreateCampaign)
